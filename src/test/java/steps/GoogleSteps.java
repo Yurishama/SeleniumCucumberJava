1c34cd5c4bade4438a02a35d4a74b7ce1d817b04
Saida Reyes <yurishama@gmail.com>
2023-04-24 15:33:06 +0200

    add scripts

--- src/test/java/steps/GoogleSteps.java
+++ src/test/java/steps/GoogleSteps.java
@@ -1,27 +1,35 @@
 package steps;
 
-import io.cucumber.java.en.*;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 import pages.GooglePage;
 
 
 public class GoogleSteps{
 
     GooglePage google = new GooglePage();
+
     @Given("^I am on the Google search page$")
     public void navigateToGoogle(){
         google.navigateToGoogle();
 
     }
 
+
     @When("^I enter a search criteria$")
     public void enterSearchCriteria(){
 
     }
 
+
     @And("^click on the search button$")
     public void clickSearchButton(){
 
     }
+
 
     @Then("^the results match the criteria$")
     public void validateResults(){
--- src/test/resources/features/Google.feature
+++ src/test/resources/features/Google.feature
@@ -1,6 +1,6 @@
-Feature: Probar la busqueda de Google
+Feature: Test Google search funcionality
 
-Scenario:Google search
+Scenario:As a user I enter a search criteria in Google
     Given I am on the Google search page
     When I enter a search criteria
     And click on the search button
